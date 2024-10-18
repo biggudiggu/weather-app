375cb59e838f8ca502623f9bdb43559513b650b6
biggudiggu <busragci@hotmail.com>
2024-10-19 01:49:47 +0200

    a lil bit logic

--- index.js
+++ index.js
@@ -9,15 +9,40 @@ const humDisDisplay = document.querySelector(".humDisDisplay");
 
 const apiKey = "65a62a648daa38a900d62be7fe8ec28a";
 
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", async event => {
+
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } 
+    catch (error) {
+      console.error(error);
+    }
+  }
+  else{
+    displayError("Please enter a city");
+  }
 
 })
 
 async function getWeatherData(city) {
-  
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  if(!response.ok){
+    throw new Error("Could not fetch weather data")
+  }
+  return await response.json();
 }
 
 function displayWeatherInfo(data) {
+  
   
 }
 
@@ -27,4 +52,11 @@ function getWeatherEmoji(weatherID) {
 
 function displayError(message) {
   
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+
+  weatherCard.textContent = "";
+  weatherCard.style.display = "flex";
+  weatherCard.appendChild(errorDisplay);
 }
