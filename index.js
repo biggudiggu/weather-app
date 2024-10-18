488a49b58f3d04c288dfdc26c117e59f90b0a983
biggudiggu <busragci@hotmail.com>
2024-10-19 01:22:54 +0200

    style done

---
+++ index.js
@@ -0,0 +1,30 @@
+// WEATHER APP  
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput"); //textbox
+const submitBtn = document.querySelector(".submitBtn"); //search button
+const weatherCard = document.querySelector(".weatherCard"); //displays the data
+const weatherContainer = document.querySelector(".weatherContainer");
+const humDisDisplay = document.querySelector(".humDisDisplay");
+
+const apiKey = "65a62a648daa38a900d62be7fe8ec28a";
+
+weatherForm.addEventListener("submit", event => {
+
+})
+
+async function getWeatherData(city) {
+  
+}
+
+function displayWeatherInfo(data) {
+  
+}
+
+function getWeatherEmoji(weatherID) {
+  
+}
+
+function displayError(message) {
+  
+}
